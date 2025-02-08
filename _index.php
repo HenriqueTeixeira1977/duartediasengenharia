607e49a8246322fa3c51a03bb00c6485fc3ba792
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2025-02-08 20:52:44 -0300

    Update20250208205229

---
+++ _index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Duarte Dias Engenharia | Pericias e Consultorias Técnicas</title>
+</head>
+<body>
+    <h1>Duarte Dias</h1>
+    
+</body>
+</html>
